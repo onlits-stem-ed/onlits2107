a2e1e7e4a223cb6530fdc29bd14ebbe8663a644b
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-04-18 19:20:55 +0530

    20230418

--- DS/Java/Recursion/Fibonacci.java
+++ DS/Java/Recursion/Fibonacci.java
@@ -1,10 +1,15 @@
 class Fibonacci {
-    int fibonacci(int n) {
+    static int fibonacci(int n) {
         if(n==0)
             return 0;
         else if(n==1)
             return 1;
         
-        return fibonacci(n-2) + fibonacci(n-1)
+        return fibonacci(n-2) + fibonacci(n-1);
+    }
+    
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++)
+            System.out.println(fibonacci(i) + " ");
     }
 }
